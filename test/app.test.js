9dbe60f364e2986b82c5449dde235e3853cabd21
Mehul Patel <mehulpatel118@gmail.com>
2019-04-02 12:53:34 -0400

    all tests

--- test/app.test.js
+++ test/app.test.js
@@ -3,20 +3,120 @@
 const app = require('../app');
 const expect = require('chai').expect;
 const request = require('supertest');
+const store = require('../playstore.js');
+
 
 describe('playstore GET /app endpoint', () => {
 
-  it('should give an object back with details about string a', () => {
+  it('return all the apps', () => {
     return request(app)
       .get('/app') // invoke the endpoint // send the query string ?n=5
-      .expect(200)  //assert that you get a 200  OK status
-      .expect('Content-Type', /json/)
       .then(res => {
-        expect(res.body).to.deep.include({'f':1,'unique':1,'average':1,'highest':'f'});
+        expect(res.body).to.eql(store);
       });
   });
 
-  it('should error out', () => {
+  
+    it('invalid sort', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'sort':'name',})
+      .expect(400, 'Can only sort by rating or app');
+  });
+
+  
+  
+  it('invalid sort valid genre', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'sort':'name',
+    'genres':'Action'})
+      .expect(400, 'Can only sort by rating or app');
+  });
+
+
+    
+  it('invalid genre', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'genres':'notgenre',})
+      .expect(400, 'can only filter by Action,Puzzle,Strategy,Casual,Arcade,or Card');
+  });
+
+
+      
+  it('valid sort invalid genre', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'genres':'notgenre',
+                'sort': 'app'})
+      .expect(400, 'can only filter by Action,Puzzle,Strategy,Casual,Arcade,or Card');
+  });
+
+  it('Valid sort', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'sort':'app',})
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        let i = 0;
+        let sorted = true;
+        while(sorted && i < res.body.length - 1) {
+          sorted = sorted && res.body[i].App < res.body[i + 1].App;
+          i++;
+        }
+        expect(sorted).to.be.true;
+      });
+  });
+
+  it('Valid sort', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'sort':'rating',})
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        let i = 0;
+        let sorted = true;
+        while(sorted && i+1 < res.body.length - 1) {
+          sorted = sorted && res.body[i].Rating >= res.body[i + 1].Rating;
+          i++;
+        }
+        expect(sorted).to.be.true;
+      });
+  });
+
+  it('filter by genre', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'genres':'Action',})
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(6);
+      });
+  });
+  
+  it('sort by app name and filter by action ', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'genres':'Action',
+            'sort':'app'})
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(6);
+        let i = 0;
+        let sorted = true;
+        while(sorted && i < res.body.length - 1) {
+          sorted = sorted && res.body[i].App < res.body[i + 1].App;
+          i++;
+        }
+        expect(sorted).to.be.true;
+
+      });
+  });
+
+
+
+  /*  it('should error out', () => {
     return request(app)
       .get('/frequency') // invoke the endpoint //assert that you get a 200  OK status
       .expect(400, 'Invalid request');
@@ -32,5 +132,6 @@ describe('playstore GET /app endpoint', () => {
         expect(res.body).to.deep.include({'z':4,'unique':1,'average':4,'highest':'z'});
       });
   });
+  */
 
 });
